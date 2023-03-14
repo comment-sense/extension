8a8a105a7d0b130f8e2604295ef8ee0d23bb93ce
George Bockari <bitwhys@gmail.com>
2023-03-14 03:43:00 -0400

    chore: initial commit

---
+++ src/popup.tsx
@@ -0,0 +1,14 @@
+import { CountButton } from "~features/count-button"
+
+import "~base.css"
+import "~style.css"
+
+function IndexPopup() {
+  return (
+    <div className="flex h-16 w-40 items-center justify-center">
+      <CountButton />
+    </div>
+  )
+}
+
+export default IndexPopup
